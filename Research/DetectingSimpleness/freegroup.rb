ba36d5ad3abafd52b8ade353fc57436f1a89974d
ryosuke <yamryo0202@gmail.com>
2019-12-11 15:26:18 +0900

    Greatly improve Research/Detecting.../freegroup.rb and Goldman....ipynb

--- Research/DetectingSimpleness/freegroup.rb
+++ Research/DetectingSimpleness/freegroup.rb
@@ -3,32 +3,21 @@
 ##  Free Group
 ##
 #------------------------------------------------------------
-module Element
-  def initialize(factors = [])
-    @factors = factors
-  end
-  attr_reader :factors
-  #---
-  def to_s() @factors.map(&:to_s).join end
-  def show() self.to_s end
-  def length() @factors.length end
-  def ==(anElement) self.show == anElement.show end
-end
-
-#------------------------------------------------------------
 class Letter
-  include Element
   #---
   def initialize(char='1')
+    '''
+      Argument: [1a-zA-Z]
+    '''
     raise ArgumentError, "You can use alphabet and '1'" unless char[0] =~ /[1a-zA-Z]/
-    super([char])
-    @char = char[0]
+    @char = char[0].downcase
+    @sign = (char[0] == @char) ? 1 : -1
   end
   attr_reader :char
   #---
   def =~(a_Letter)
     raise ArgumentError unless a_Letter.is_a? Letter
-    @char.downcase == a_Letter.char.downcase
+    @char == a_Letter.char
   end
   def ==(a_Letter)
     raise ArgumentError unless a_Letter.is_a? Letter
@@ -43,140 +32,114 @@ class Letter
     self.show <=> another.show
   end
   #---
+  def to_s
+    (@sign == 1) ? @char : @char.upcase
+  end
+  def show() self.to_s end
+  #---  
   def inverse
     if @char == '1' then
       self.dup
     else
-      inv_char = (self.inverse?) ? @char.downcase : @char.upcase
-      self.class.new(inv_char)
+      self.class.new((@sign == 1) ? @char.upcase : @char)
     end
   end
-  def inverse?() (@char == '1') ? nil : (@char == @char.upcase) end
+  def inverse?() (@char == '1') ? nil : (@sign == 1) end
   def inverse_of?(a_Letter)
-    (@char == '1') ? (a_Letter.char == '1') : (self =~ a_Letter) && !(self == a_Letter)
+    (@char == '1') ? (a_Letter.char == '1') : (self =~ a_Letter) && (self.sign != a_Letter.sign)
   end
 end
 
 #------------------------------------------------------------
-class Word
-  include Element
+class Word < Array
   #---
-  # Word class consists of a (nested) array of Letter objects.
+  # Word class which is a (nested) array of Letters.
   #---
   def initialize(*args)
     args.flatten!
-    @factors = []
-    if args.empty?
-      @factors << Letter.new('1')
-    else
-      args.each do |arg|
-        @factors << case arg
-                    when Letter then
-                      arg
-                    when String then
-                      subarr = []
-                      arg.each_char{|c| subarr << Letter.new(c) if c =~ /[1a-zA-Z]/}
-                      if subarr.empty?
-                        Letter.new('1')
-                       elsif subarr.length == 1
-                         subarr[0]
-                      else
-                        subarr
-                      end
-                    else raise ArgumentError, arg.class
-                    end
-      end
-      @factors = @factors[0] if (@factors.length == 1 and @factors[0].is_a?(Array))
+    args.each do |arg|
+      self << case arg
+               when Letter then
+                 arg
+               when String then
+                  arg.each_char.map{|c| Letter.new(c)} unless arg.empty?
+               else
+                  raise ArgumentError, arg.class
+               end
     end
-#     super(factors)
+    self << Letter.new if self.empty?
+    self.flatten! if self.size == 1 && self[0].is_a?(Array)
   end
-  attr_accessor :factors
   #---
-  def pop() @factors.pop end
-  def letter_at(int) @factors[int] end
-  def [](int) self.letter_at(int) end
-  def first() @factors.flatten.first end
-  def last() @factors.flatten.last end
+  def letter_at(int) (self.flatten)[int] end
+  def first_letter() (self.flatten).first end
+  def last_letter() (self.flatten).last end
   #---
   def count(a_letter)
-    a_letter = Letter.new(a_letter) if a_letter.is_a? String
-    return @factors.count(a_letter)
+    a_letter = Letter.new(a_letter) if a_letter.is_a?(String)
+    return (self.flatten).count(a_letter)
   end
-  #---
-  def flatten
-    flat_factors = @factors.flatten
-    self.class.new(flat_factors)
-  end
-  def flatten!() @factors.flatten!; return self end
   #---
   def split(num)
-    raise ArgumentError unless num.is_a? Integer and num > 0
-    div = [@factors[0..num-1], @factors[num..-1]]
+    raise ArgumentError unless ( num.is_a?(Integer) && (0..self.size).include?(num) )
+    div = (num == 0) ? [Letter.new, self] : [self[0..num-1], self[num..-1]]
     return div.map{|sub| self.class.new(sub)}
   end     
   #---
-  def show() show_parens(@factors).gsub(/[()]+/, '.').gsub(/^[.]|[.]$/, '') end 
+  def show() show_parens(self).gsub(/[()]+/, '.').gsub(/^[.]|[.]$/, '') end 
   def show_parens(myarr)
-    myarr.map{|f| (f.is_a?(Array)) ? "(#{self.show_parens(f)})" : f.show }.join
+    myarr.map{|f| (f.is_a? Array) ? "(#{self.show_parens(f)})" : f.show }.join
   end
   #---
-  def inverse
-      return self.class.new(@factors.flatten.reverse.map(&:inverse))
-  end
+  def inverse() self.class.new((self.flatten).reverse.map(&:inverse)) end
+  #---
   def deep_copy
-    facs_copy = []
-    for f in @factors
-      facs_copy << f
-    end
-    rtn = Word.new
-    rtn.factors = facs_copy
-    return rtn
+    str_arr = self.map{|letter| letter.to_s}
+    return self.class.new(str_arr)
   end
   #---
   def contract_once
-    if @factors.size > 1
-      left = []
-      right = self.flatten.factors
+    arr = self.dup
+    if arr.size > 1
+      left, right = self.class.new, arr.flatten
       while right.size > 1
-        left << right.shift
+        left *= right.shift
         if left.last.inverse == right.first
           left.pop
           right.shift
         end
       end
-      left += right
-      @factors = (left.empty?) ? [Letter.new] : left
+      left *= right
     end
-    return self
+    return left
   end
   def contract
-    self.flatten!
+    w = self.dup
     size_diff = 1
-    while (size_diff > 0 and @factors.size > 1)
-      size_diff = @factors.size - self.contract_once.factors.size
+    while (size_diff > 0 && w.size > 1)
+      before = w.size
+      w = w.contract_once
+      after = w.size
+      size_diff = before - after
     end
-    #
-    return self
+    return w
   end
   #---
   def cyclic_permutation(num=1)
-    letters = @factors.flatten
-    former = letters[0, num]
-    latter = letters.drop(num)
-    return self.class.new(latter + former)
+    self.class.new((self.flatten).rotate(num))
   end
   def cyclic_reduce
     ww = (self.dup).contract 
-    wcp = Group::Identity
+    wcp = self.class.new
     while (wcp.size < ww.size)
-      ww.factors = wcp.factors unless wcp == Group::Identity
+      ww = wcp unless wcp.show == '1'
       wcp = ww.cyclic_permutation.contract
     end
     return ww
   end
-  def conjugate?(aWord)
+  def is_cyclically_same?(aWord)
     words = [self, aWord].map(&:cyclic_reduce)
-    (0..words[0].size-1).map do |i|
+    (0..words[0].size-1).each do |i|
         if (words[0].cyclic_permutation(i) == words[1])
           return true
           break
@@ -186,39 +149,43 @@ class Word
   end
   #---
   def *(anElement)
-    raise ArgumentError unless anElement.is_a? Element
-    pd = self.dup
-    pd.factors = [pd.factors, anElement.factors]
-    pd.factors.delete(Letter.new)
-    return pd
+    letters = self.map{|v| v}
+    case anElement
+    when Letter
+      letters << anElement
+    when Word
+      letters += anElement  #NOTE: As a result, the variable letters is an Array.
+    else
+      raise ArgumentError, anElement.class
+    end
+    letters.delete(Letter.new)
+    return (letters.empty?) ? Word.new : self.class.new(letters)
   end
   def ^(idx)
-    raise ArgumentError unless (idx.is_a?(Integer) and idx >= 0)
-    power = self.dup
-    (idx-1).times do |k|
-      power *= self
-    end
-    return power
-  end
-  def size
-    return @factors.flatten.size
+    raise ArgumentError unless (idx.is_a?(Integer) and idx >= 0)    
+    return idx.times.map{|k| self}.inject{|pd, w| pd*w}
   end
 end
-
+      
 #------------------------------------------------------------
 module Group extend self
   Identity = Word.new('1')
   #---
   def product(element1,element2)
-    raise ArgumentError unless [element1, element2].all?{|elm| elm.is_a? Element}
-    return Word.new(element1.factors, element2.factors)
+    words = to_word(element1, element2)
+    return words[0]*words[1]
   end
   def commutator(element1, element2)
-    raise ArgumentError unless [element1, element2].all?{|elm| elm.is_a? Element}
-    return Word.new(element1.factors, element2.factors, element1.inverse.factors, element2.inverse.factors)
+    words = to_word(element1, element2)
+    return words[0]*words[1]*(words[0].inverse)*(words[1].inverse)
   end
   def conjugate(element1, element2)
-    raise ArgumentError unless [element1, element2].all?{|elm| elm.is_a? Element}
-    return Word.new(element2.factors, element1.factors, element2.inverse.factors)
+    words = to_word(element1, element2)
+    return words[1]*words[0]*(words[1].inverse)
+  end
+  private
+  def to_word(*elements)
+    raise ArgumentError unless elements.all?{|elm| (elm.is_a? Word) || (elm.is_a? Letter)}
+    elements.map{|elm| (elm.is_a? Word) ? elm : Word.new(elm)}
   end
 end
